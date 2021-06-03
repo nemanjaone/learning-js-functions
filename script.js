6637ab0837ca7d8e26aa94cc7c8415fe2498471f
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-06-02 22:57:07 -0400

    leetcode twosum solution

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function rant(message) {
-    console.log(message.toUpperCase());
-}
+const nums = [3, 3];
+const target = 6;
 
-for (let i = 0; i < 3; i++) {
-    rant('woop');
-}
+var twoSum = function (nums, target) {
+    let cur = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let a = nums[i];
+        for (let j = 1; j < nums.length; j++) {
+            let b = nums[j];
+            cur = a + b;
+            if (i != j) {
+                if (cur === target) {
+                    console.log(i, j);
+                    retArray = [i, j];
+                    return retArray;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+};
+
+twoSum(nums, target);
