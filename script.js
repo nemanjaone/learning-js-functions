c9d0de5843a175ca8588f10c68ec3e760458da08
Nemanja Blagojevic <zzirnemus@gmail.com>
2021-06-04 14:10:56 -0400

    function scope

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-const nums = [3, 3];
-const target = 6;
+let eggs = 0;
+function collecteggs() {
+    eggs = 6;
+}
 
-var twoSum = function (nums, target) {
-    let cur = 0;
-    for (let i = 0; i < nums.length; i++) {
-        let a = nums[i];
-        for (let j = 1; j < nums.length; j++) {
-            let b = nums[j];
-            cur = a + b;
-            if (i != j) {
-                if (cur === target) {
-                    console.log(i, j);
-                    retArray = [i, j];
-                    return retArray;
-                }
-            } else {
-                break;
-            }
-        }
-    }
-};
-
-twoSum(nums, target);
+console.log(eggs);
+collecteggs();
+console.log(eggs);
